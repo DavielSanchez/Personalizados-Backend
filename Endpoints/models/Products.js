a3722def93962de283d9e57ef7bd0a92a5e25989
Daviel Alexander Sanchez <Davielalexsanchez@gmail.com>
2024-10-13 08:57:09 -0400

    Agregando campos faltantes al modelo de Productos

--- Endpoints/models/Products.js
+++ Endpoints/models/Products.js
@@ -5,13 +5,16 @@ const productsSchema = mongoose.Schema({
     productPrice: { type: Number, required: false },
     productTag: { type: String, required: false },
     productColors: { type: Array, required: false },
+    productSizes: { type: Array, required: false },
     productSummary: { type: String, required: false },
     productDescription: { type: String, required: false },
+    productMainImage: { type: Array, required: false },
     productImages: { type: Array, required: false },
     productStock: { type: Number, required: false, default: 0 },
     productOffer: { type: Boolean, required: false, default: false },
     productDiscount: { type: Number, required: false, default: 0 },
     productCategory: { type: Schema.Types.ObjectId, ref: 'Categories' },
+    // productLikes: { type: Number, required: false, default: 0 },
     productComment: { type: String, required: false }
 })
 

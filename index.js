422911dc6e36c3abaffa9d492581d7675ee0aca5
Daviel Alexander Sanchez <Davielalexsanchez@gmail.com>
2025-01-13 22:37:27 -0400

    Agregando NewsLetter y filtros en productos3.

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ mongoConnection(process.env.MONGODB_URI);
 
 
 // MIDDLEWARES //
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://blanchedalmond-kingfisher-785257.hostingersite.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json())
 app.use('/', Categories)
 app.use('/', Products)
